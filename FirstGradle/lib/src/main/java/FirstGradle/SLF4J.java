82fc549862c41a7d8776d9aba490e604547187c5
chen84928 <chen84928@DESKTOP-SDNF9DJ.cdsafi.com>
2022-04-19 14:15:11 +0800

    First-Gradle

---
+++ FirstGradle/lib/src/main/java/FirstGradle/SLF4J.java
@@ -0,0 +1,18 @@
+package FirstGradle;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class SLF4J {
+private static Logger LOGGER = LoggerFactory.getLogger(SLF4J.class);
+
+
+
+public static void main(String[] args) {
+LOGGER.info("Hello World");
+//System.out.println("Hello World");
+}
+}
